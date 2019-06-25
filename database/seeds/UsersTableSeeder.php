97417cddd507f5ce8ab78a0d13d8b3d171cb6f65
Toan <toanhuynh345@gmail.com>
2019-06-25 16:52:16 +0700

    layouts

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => 'thanhtoan@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
